381b2f9246bffa6029c0ab1717bb1f72f8d3799d
RLMarvin <35970829+RLMarvin@users.noreply.github.com>
2019-01-05 20:55:41 +0000

    Merge upstream master & cleanup. (#4)

--- util/coordinates.py
+++ util/coordinates.py
@@ -4,14 +4,6 @@ import math
 PI = math.pi
 
 
-def spherical(vec: vec3) -> vec3:
-    """Converts from cartesian to spherical coordinates."""
-    radius = norm(vec) + 1e-9
-    inclination = math.acos(vec[2] / radius)
-    azimuth = math.atan2(vec[1], vec[0])
-    return vec3(radius, Range180(PI / 2 - inclination), azimuth)
-
-
 def Range180(a: float, pi: float = PI) -> float:
     """Limits any angle a to [-pi, pi] range, example: Range180(270, 180) = -90"""
     if abs(a) >= 2 * pi:
@@ -19,3 +11,11 @@ def Range180(a: float, pi: float = PI) -> float:
     if abs(a) > pi:
         a -= 2 * pi * math.copysign(1, a)
     return a
+
+
+def spherical(vec: vec3) -> vec3:
+    """Converts from cartesian to spherical coordinates."""
+    radius = norm(vec) + 1e-9
+    inclination = math.acos(vec[2] / radius)
+    azimuth = math.atan2(vec[1], vec[0])
+    return vec3(radius, Range180(PI / 2 - inclination), azimuth)
