Fix signs and improve TurnFaceTarget testing.

mechanic/drive_arrive_in_time/drive_agent.py
@@ -21,7 +21,7 @@ class TestAgent(BaseTestAgent):
 
         my_car_loc = self.game_data.my_car.location
         game_time = self.game_data.time
-        hit_radius = self.game_data.ball.radius + np.min(np.abs(self.game_data.my_car.hitbox))
+        hit_radius = self.game_data.ball.radius + np.min(np.abs(self.game_data.my_car.hitbox)) / 2
 
         # only accurate if we're already moving towards the target
         velocity = np.array([np.linalg.norm(self.game_data.my_car.velocity)])

mechanic/drive_arrive_in_time/drive_arrive_in_time.py
@@ -24,10 +24,10 @@ class DriveArriveInTime(BaseMechanic):
         # PD for steer
         yaw_angle_to_target = math.atan2(target_in_local_coords[1], target_in_local_coords[0])
         car_ang_vel_local_coords = np.dot(car.angular_velocity, car.rotation_matrix)
-        car_yaw_ang_vel = car_ang_vel_local_coords[2]
+        car_yaw_ang_vel = -car_ang_vel_local_coords[2]
 
         proportional_steer = 11 * yaw_angle_to_target
-        derivative_steer = -1 / 3 * car_yaw_ang_vel
+        derivative_steer = 1 / 3 * car_yaw_ang_vel
 
         if sign(yaw_angle_to_target) * (yaw_angle_to_target + car_yaw_ang_vel / 3) > PI / 5:
             self.controls.handbrake = True

mechanic/drive_turn_face_target/drive_turn_face_target.py
@@ -17,10 +17,10 @@ class DriveTurnFaceTarget(BaseMechanic):
         # PD for steer
         yaw_angle_to_target = math.atan2(target_in_local_coords[1], target_in_local_coords[0])
         car_ang_vel_local_coords = np.dot(car.angular_velocity, car.rotation_matrix)
-        car_yaw_ang_vel = car_ang_vel_local_coords[2]
+        car_yaw_ang_vel = -car_ang_vel_local_coords[2]
 
         proportional_steer = 11 * yaw_angle_to_target
-        derivative_steer = -1 / 3 * car_yaw_ang_vel
+        derivative_steer = 1 / 3 * car_yaw_ang_vel
 
         if sign(yaw_angle_to_target) * (yaw_angle_to_target + car_yaw_ang_vel / 3) > PI / 5:
             self.controls.handbrake = True
@@ -42,13 +42,15 @@ class DriveTurnFaceTarget(BaseMechanic):
         # rendering
         strings = [f"yaw_angle_to_target : {yaw_angle_to_target:.2}",
                    f"car_yaw_ang_vel : {car_yaw_ang_vel:.2}",
-                   f"steer : {self.controls.steer:.2f}"]
+                   f"steer : {self.controls.steer:.2f}",
+                   f"handbrake : {self.controls.handbrake}"]
+        color = self.agent.renderer.white()
 
         self.agent.renderer.begin_rendering()
         for i, string in enumerate(strings):
-            self.agent.renderer.draw_string_2d(20, 150 + i * 40, 2, 2, string,
-                                               self.agent.renderer.white())
+            self.agent.renderer.draw_string_2d(20, 150 + i * 40, 2, 2, string, color)
         self.agent.renderer.draw_rect_3d(target_loc, 20, 20, True, self.agent.renderer.red())
+        self.agent.renderer.draw_line_3d(car.location, target_loc, color)
         self.agent.renderer.end_rendering()
 
         # updating status

skeleton/util/structure/game_data.py
@@ -101,7 +101,11 @@ class GameData:
         buffer = buf_from_mem(ctypes.addressof(game_boosts), dtype.itemsize * num_boosts, 0x100)
         converted_game_boosts = np.frombuffer(buffer, dtype)
 
-        self.boost_pads[['is_active', 'timer']] = converted_game_boosts
+        try:
+            self.boost_pads[['is_active', 'timer']] = converted_game_boosts
+        except TypeError as e:
+            print("Make sure you have called SkeletonAgent's initialize_agent()")
+            raise e
 
     def read_game_info(self, game_info: GameInfo):
 