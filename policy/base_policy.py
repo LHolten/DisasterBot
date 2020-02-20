a395c543de5a305f31c7da397f49c40d1f4b4cef
Hytak <lhc.holten@gmail.com>
2020-02-20 16:32:57 +0100

    Implement example generator policy

--- policy/base_policy.py
+++ policy/base_policy.py
@@ -5,14 +5,9 @@ class BasePolicy:
     def __init__(self, agent, rendering_enabled=False):
         self.agent = agent
         self.rendering_enabled = rendering_enabled
-        self.action: BaseAction = self.get_default_action()
 
     def get_controls(self, game_data):
-        self.action = self.get_action(game_data)
-        return self.action.get_controls(game_data)
-
-    def get_default_action(self) -> BaseAction:
-        raise NotImplementedError
+        return self.get_action(game_data).get_controls(game_data)
 
     def get_action(self, game_data) -> BaseAction:
         raise NotImplementedError
--- policy/example_generator_policy/example_generator_policy.py
+++ policy/example_generator_policy/example_generator_policy.py
@@ -7,6 +7,8 @@ from action.collect_boost import CollectBoost
 from action.kickoff import Kickoff
 from action.hit_ground_ball import HitGroundBall
 
+from typing import Generator
+
 
 class ExamplePolicy(BasePolicy):
     def __init__(self, agent, rendering_enabled=True):
@@ -14,28 +16,31 @@ class ExamplePolicy(BasePolicy):
         self.collect_boost_action = CollectBoost(agent, rendering_enabled)
         self.kickoff_action = Kickoff(agent, rendering_enabled)
         self.hit_ball_action = HitGroundBall(agent, rendering_enabled)
-        self.action = self.kickoff_action
+        self.action = None
 
     def get_action(self, game_data) -> BaseAction:
-
         ball_loc = game_data.ball.location
         kickoff = math.sqrt(ball_loc[0] ** 2 + ball_loc[1] ** 2) < 1
 
-        interrupt = kickoff
-
-        # keep doing the same action until finished or interrupted
-        if not self.action.finished and not interrupt:
-            return self.action
-
-        self.action.reset_status()
-
-        # simple decision tree to choose the next action
         if kickoff:
-            self.action = self.kickoff_action
+            # reset the action loop
+            self.action = None
+            return self.kickoff_action
         else:
-            if game_data.my_car.boost > 20:
-                self.action = self.hit_ball_action
-            else:
-                self.action = self.collect_boost_action
+            if self.action is None:
+                self.action = self.action_loop(game_data)
+            return self.action.send(game_data)
 
-        return self.action
+    def action_loop(self, game_data) -> Generator[BaseAction]:
+        while True:
+            # choose action to do
+            if game_data.my_car.boost > 20:
+                action = self.collect_boost_action
+            else:
+                action = self.collect_boost_action
+
+            # use action until it is finished
+            while not action.finished:
+                game_data = yield action
+
+            action.reset_status()
