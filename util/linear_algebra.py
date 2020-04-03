8295c58e129b8f37eac0094e2742d3f7c29720e2
oxrock <oxrock2001@yahoo.com>
2020-04-03 07:02:34 -0400

    added 2 new handy functions to the linear alg utilities

--- util/linear_algebra.py
+++ util/linear_algebra.py
@@ -17,3 +17,13 @@ def normalize(vec: np.ndarray):
 
 def normalize_batch(vec: np.ndarray):
     return vec / np.maximum(np.linalg.norm(vec, axis=-1), 1e-8)
+
+
+def angle_between_vectors(vec1: np.ndarray, vec2: np.ndarray):
+    """Provides angle between 2 vectors in radians"""
+    return dot(normalize(vec1), normalize(vec2)) * math.pi
+
+
+def optimal_intercept_vector(collider_location: np.ndarray, collider_velocity: np.ndarray, target_location: np.ndarray):
+    """Provides vector for correcting an object's velocity vector towards the target vector"""
+    return normalize(normalize(collider_velocity) + normalize(collider_location - target_location))
