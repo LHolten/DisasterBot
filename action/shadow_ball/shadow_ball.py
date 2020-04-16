7a3a07b5f6653020a1b5b78ca6bad1384ff55cf9
Hytak <lhc.holten@gmail.com>
2020-04-16 22:02:30 +0200

    Update rotations

--- action/shadow_ball/shadow_ball.py
+++ action/shadow_ball/shadow_ball.py
@@ -7,14 +7,15 @@ from util.linear_algebra import normalize, norm, dot
 
 
 class ShadowBall(BaseAction):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.mechanic = DriveNavigateBoost(self.agent, self.rendering_enabled)
+    def __init__(self, agent, rendering_enabled, distance):
+        super().__init__(agent, rendering_enabled)
+        self.mechanic = DriveNavigateBoost(agent, rendering_enabled)
+        self.distance = distance
 
     def get_controls(self, game_data: GameData) -> SimpleControllerState:
         dt = norm(game_data.own_goal.location - game_data.ball.location) / 6000
         ball_location = game_data.ball_prediction[int(60 * dt)]["physics"]["location"]
-        target_loc = normalize(game_data.own_goal.location - ball_location) * 1200 + ball_location
+        target_loc = normalize(game_data.own_goal.location - ball_location) * self.distance + ball_location
         up = game_data.my_car.rotation_matrix[:, 2]
         target_loc = target_loc - dot(target_loc - game_data.my_car.location, up) * up
         target_dir = game_data.ball.velocity - dot(game_data.ball.velocity, up) * up
--- policy/tournament_policy/tournament_policy.py
+++ policy/tournament_policy/tournament_policy.py
@@ -11,38 +11,35 @@ from action.shoot_at_goal import ShootAtGoal
 from policy.base_policy import BasePolicy
 from skeleton.util.structure import GameData
 from util.linear_algebra import norm
+from util.numerics import sign
 from util.physics.drive_1d_heuristic import state_at_distance_heuristic
 from util.team_utilities import kickoff_decider
 
 
-def get_ball_control(game_data):
+def get_ball_control(game_data: GameData):
     own_time, _, _ = state_at_distance_heuristic(
         game_data.my_car.location - game_data.ball.location, game_data.my_car.velocity, game_data.my_car.boost
     )
-    teammate_time = 0
-    if len(game_data.teammates) > 1:
-        teammate_time = max(
-            state_at_distance_heuristic(
-                teammate["physics"]["location"].astype(float) - game_data.ball.location,
-                teammate["physics"]["velocity"].astype(float),
-                teammate["boost"].astype(float),
-            )[0]
-            for teammate in game_data.teammates
-            if teammate["spawn_id"] != game_data.my_car.spawn_id
-        )
+    team_sign = sign(game_data.team)
 
-    opponent_time = np.inf
-    if len(game_data.opponents) > 0:
-        opponent_time = min(
-            state_at_distance_heuristic(
-                opponent["physics"]["location"].astype(float) - game_data.ball.location,
-                opponent["physics"]["velocity"].astype(float),
-                opponent["boost"].astype(float),
-            )[0]
-            for opponent in game_data.opponents
-        )
+    def is_active_team(teammate):
+        teammate_location = teammate["physics"]["location"].astype(float)
+        return (game_data.ball.location[1] - teammate_location[1]) * team_sign < 0
 
-    return own_time, teammate_time, opponent_time
+    def time_to_ball(player):
+        return state_at_distance_heuristic(
+            player["physics"]["location"].astype(float) - game_data.ball.location,
+            player["physics"]["velocity"].astype(float),
+            player["boost"].astype(float),
+        )[0]
+
+    teammates = list(filter(is_active_team, game_data.teammates))
+
+    teammate_time = min([*map(time_to_ball, teammates), np.inf])
+    teammate_max = max([*map(time_to_ball, teammates), 0])
+    opponent_time = min([*map(time_to_ball, game_data.opponents), np.inf])
+
+    return own_time, teammate_time, opponent_time, len(teammates), teammate_max
 
 
 class TournamentPolicy(BasePolicy):
@@ -51,7 +48,8 @@ class TournamentPolicy(BasePolicy):
         self.kickoff_action = Kickoff(agent, rendering_enabled)
         self.attack = ShootAtGoal(agent, rendering_enabled)
         self.hit_ball = HitGroundBall(agent, rendering_enabled)
-        self.shadow = ShadowBall(agent, rendering_enabled)
+        self.shadow = ShadowBall(agent, rendering_enabled, 1500)
+        self.shadow2 = ShadowBall(agent, rendering_enabled, 3000)
         self.collect_boost = CollectBoost(agent, rendering_enabled)
 
     def get_action(self, game_data: GameData) -> BaseAction:
@@ -64,14 +62,14 @@ class TournamentPolicy(BasePolicy):
             else:
                 return self.shadow
         else:
-            own, team, opp = get_ball_control(game_data)
-            if (
-                own < team
-                or (own < opp and team == 0)
-                or norm(game_data.own_goal.location - game_data.ball.location) < 3000
+            own, team, opp, num_active, team_max = get_ball_control(game_data)
+            if own == team and (
+                num_active > 1 or own < opp or norm(game_data.own_goal.location - game_data.ball.location) < 3000
             ):
                 if self.attack.finished:
                     self.attack = ShootAtGoal(self.agent, self.rendering_enabled)
                 return self.attack
             else:
+                if num_active > 2 and own == team_max:
+                    return self.shadow2
                 return self.shadow
