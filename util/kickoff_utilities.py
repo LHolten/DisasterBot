1fb6e0d8a101fcca0a8d10b0f141545c2ccb36fd
oxrock <oxrock2001@yahoo.com>
2020-05-01 15:13:58 -0400

    kickoffs updated (#8)
    * renamed team utilities to kickoff utilities and added function to determine kickoff type
    
    * updated kickoff to use drive_turn_face_target and fixed the kickoffs I had broken in the previous update

--- util/kickoff_utilities.py
+++ util/kickoff_utilities.py
@@ -9,3 +9,13 @@ def kickoff_decider(game_data: GameData) -> bool:
             if np.linalg.norm(ally[0][0] - game_data.ball.location) < my_distance:
                 return False
     return True
+
+
+def get_kickoff_position(position: np.array):
+    # kickoff_locations = [[2048, 2560], [256, 3848], [0, 4608]]
+    if abs(position[0]) >= 300:
+        return 0  # wide diagonal
+    elif abs(position[0]) > 5:
+        return 1  # short diagonal
+    else:
+        return 2  # middle
